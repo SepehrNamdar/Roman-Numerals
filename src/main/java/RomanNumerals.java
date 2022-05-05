4e3ea60e954d865ba3cbff6de49af838aecf580e
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-05-05 09:54:36 +0200

    final solution

--- src/main/java/RomanNumerals.java
+++ src/main/java/RomanNumerals.java
@@ -1,8 +1,48 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RomanNumerals {
 
-    public String convert(int arabicNumber) {
-        // TODO : Write a program to convert Arabic numbers into their Roman numeral equivalents
-        return null;
+    private final Map<Integer, String> romanNumeralSymbols;
+    private int arabicNumber;
+
+    public RomanNumerals(int arabicNumber) {
+        this.arabicNumber = arabicNumber;
+        romanNumeralSymbols = new LinkedHashMap<>();
+        romanNumeralSymbols.put(1000, "M");
+        romanNumeralSymbols.put(900, "CM");
+        romanNumeralSymbols.put(500, "D");
+        romanNumeralSymbols.put(400, "CD");
+        romanNumeralSymbols.put(100, "C");
+        romanNumeralSymbols.put(90, "XC");
+        romanNumeralSymbols.put(50, "L");
+        romanNumeralSymbols.put(40, "XL");
+        romanNumeralSymbols.put(10, "X");
+        romanNumeralSymbols.put(9, "IX");
+        romanNumeralSymbols.put(5, "V");
+        romanNumeralSymbols.put(4, "IV");
+        romanNumeralSymbols.put(1, "I");
+    }
+
+    public String convert() {
+        StringBuilder romanNumeral = new StringBuilder();
+
+        for (Integer number : romanNumeralSymbols.keySet()) {
+            final StringBuilder converted = findSymbol(number);
+            romanNumeral.append(converted);
+        }
+
+        return romanNumeral.toString();
+    }
+
+    private StringBuilder findSymbol(int number) {
+        StringBuilder romanNumeral = new StringBuilder();
+        while (arabicNumber >= number) {    // Transformation Priority Premise
+            final String romanSymbol = romanNumeralSymbols.get(number);
+            romanNumeral.append(romanSymbol);
+            arabicNumber -= number;
+        }
+        return romanNumeral;
     }
 
 }
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -4,12 +4,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralsTest {
 
+    // Single Assertion Principle
     @Test
-    void should_convert_Roman_Numeral_To_Arabic_Number() {
-        RomanNumerals romanNumerals = new RomanNumerals();
+    void should_convert_Arabic_Numeral_To_Roman_Number() {
 
-        String result = romanNumerals.convert(1);
+        assertThat(new RomanNumerals(1).convert()).isEqualTo("I");
+        assertThat(new RomanNumerals(2).convert()).isEqualTo("II");
+        assertThat(new RomanNumerals(3).convert()).isEqualTo("III");
 
-        assertThat(result).isEqualTo("I");
+        assertThat(new RomanNumerals(5).convert()).isEqualTo("V");
+        assertThat(new RomanNumerals(6).convert()).isEqualTo("VI");
+        assertThat(new RomanNumerals(7).convert()).isEqualTo("VII");
+        assertThat(new RomanNumerals(8).convert()).isEqualTo("VIII");
+
+        assertThat(new RomanNumerals(10).convert()).isEqualTo("X");
+        assertThat(new RomanNumerals(11).convert()).isEqualTo("XI");
+        assertThat(new RomanNumerals(12).convert()).isEqualTo("XII");
+        assertThat(new RomanNumerals(13).convert()).isEqualTo("XIII");
+
+        assertThat(new RomanNumerals(20).convert()).isEqualTo("XX");
+        assertThat(new RomanNumerals(21).convert()).isEqualTo("XXI");
+        assertThat(new RomanNumerals(28).convert()).isEqualTo("XXVIII");
+        assertThat(new RomanNumerals(25).convert()).isEqualTo("XXV");
+
+        assertThat(new RomanNumerals(30).convert()).isEqualTo("XXX");
+        assertThat(new RomanNumerals(37).convert()).isEqualTo("XXXVII");
+
+        assertThat(new RomanNumerals(50).convert()).isEqualTo("L");
+        assertThat(new RomanNumerals(60).convert()).isEqualTo("LX");
+        assertThat(new RomanNumerals(70).convert()).isEqualTo("LXX");
+        assertThat(new RomanNumerals(80).convert()).isEqualTo("LXXX");
+        assertThat(new RomanNumerals(87).convert()).isEqualTo("LXXXVII");
+
+        assertThat(new RomanNumerals(652).convert()).isEqualTo("DCLII");
+        assertThat(new RomanNumerals(1549).convert()).isEqualTo("MDXLIX");
+
     }
+
 }
